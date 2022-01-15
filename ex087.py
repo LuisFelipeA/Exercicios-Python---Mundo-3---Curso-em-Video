3860c648ab5268cd8fe445fe0ce5aba5a14ed3d0
LuisFelipeA <felipeadriani2@gmail.com>
2022-01-15 11:46:34 -0300

    matriz 3x3 aprimorada

--- ex087.py
+++ ex087.py
@@ -3,28 +3,68 @@
 # b) A soma dos valores da terceira coluna.
 # c) O maior valor da segunda linha.
 
+
+# entrada de dados
+
 matriz = [[],[],[]]
 
-for coluna in range(len(matriz)):
-    for linha in range(3):
+for linha in range(3):
+    for coluna in range(3):
         n1 = float(input(f"Digite um valor para adicionar na matriz: "))
-        matriz[coluna].append(n1)
-
+        matriz[linha].append(n1)
 
 print("-"*30)
 
-for coluna in range(len(matriz)):
-    for linha in range(3):
+# mostrar matriz
+
+for linha in range(3):
+    for coluna in range(3):
         print(f"[{matriz[linha][coluna]:^7}]", end=" ")
     print()
 
 print("-"*30)
 
-pares = []
+# soma dos numeros pares
 
-for coluna in range(len(matriz)):
-    for linha in range(len(matriz[coluna])):
-        if matriz[coluna][linha] % 2 == 0:
-            pares.append(matriz[coluna][linha])
+paresSoma = 0
 
-print(f"os numeros pares são {pares}")
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz[linha])):
+        if matriz[linha][coluna] % 2 == 0:
+            paresSoma = paresSoma + matriz[linha][coluna]
+
+print(f"A soma dos numeros pares é {paresSoma}")
+
+print("-"*30)
+
+# soma dos numeros da terceira coluna
+
+terceiraColuna = 0
+
+for linha in range(len(matriz)):
+    for coluna in range(len(matriz[linha])):
+        if matriz[linha][coluna] == matriz[linha][2]:
+            terceiraColuna = terceiraColuna + matriz[linha][coluna]
+
+print(f"A soma da terceira coluna é {terceiraColuna}")
+
+print("-"*30)
+
+# maior valor da segunda linha
+
+maiorSegundaLinha = 0
+
+for linha in range(len(matriz)):
+    if matriz[linha] == matriz[1]:
+            for coluna in range(len(matriz[linha])):
+
+                if maiorSegundaLinha == 0:
+                    maiorSegundaLinha = matriz[linha][coluna]
+
+                else:
+                    if maiorSegundaLinha <= matriz[linha][coluna]:
+                        maiorSegundaLinha = matriz[linha][coluna]
+
+print(f"O maior numero da segunda linha é {maiorSegundaLinha}") 
+
+print("-"*30)
